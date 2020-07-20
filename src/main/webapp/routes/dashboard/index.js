a9c402ba42affdf2302ff86adb12355baf20a1df
Prabin <upretiprabin7946@gmail.com>
2020-07-20 16:55:01 +0545

    Dashboard design why us

--- src/main/webapp/routes/dashboard/index.js
+++ src/main/webapp/routes/dashboard/index.js
@@ -60,8 +60,8 @@ class Index extends Component {
         } = this.state;
         const countryList = [
             {
-            'id' : 1,
-            'name' : 'Nepal'
+                'id' : 1,
+                'name' : 'Nepal'
             }
         ];
 
@@ -83,6 +83,48 @@ class Index extends Component {
                     </div>
                     <h5>Today's Rate : 83.2900 NPR</h5>
                     <MatButton className="btn btn-primary mt-10">Continue</MatButton>
+                </div>
+                <div className="choose">
+                    <Card body>
+                        <h2 className={"text-9 text-center mt-60"}>Why should you choose Hamro Remit?</h2>
+                        <p className="text-4 text-center mb-5">Here’s Top 4 reasons why</p>
+                        <div className="mt-30 container">
+                            <div className="row">
+                                <div className="col-sm-6 col-lg-3 mb-5 mb-lg-0">
+                                    <div className="featured-box">
+                                        <div className="featured-box-icon text-primary">
+                                            <i className="fa fa-hand-pointer-o fa-lg" aria-hidden="true"/>                                        </div>
+                                        <h3>Easy to use</h3>
+                                        <p className="text-3">We provide sophisticated UI which makes your transaction smooth and easy </p>
+                                    </div>
+                                </div>
+                                <div className="col-sm-6 col-lg-3 mb-5 mb-lg-0">
+                                    <div className="featured-box">
+                                        <div className="featured-box-icon text-primary">
+                                            <i className="fa fa-share fa-lg" aria-hidden="true"/>                                        </div>
+                                        <h3>Faster Payments</h3>
+                                        <p className="text-3">We have developed our system to be quick. We won't keep you waiting for long.</p>
+                                    </div>
+                                </div>
+                                <div className="col-sm-6 col-lg-3 mb-5 mb-lg-0">
+                                    <div className="featured-box">
+                                        <div className="featured-box-icon text-primary">
+                                            <i className="fa fa-dollar fa-lg" aria-hidden="true"/>                                        </div>
+                                        <h3>Lower Fees</h3>
+                                        <p className="text-3">We charge very reasonable service to our customer.</p>
+                                    </div>
+                                </div>
+                                <div className="col-sm-6 col-lg-3 mb-5 mb-lg-0">
+                                    <div className="featured-box">
+                                        <div className="featured-box-icon text-primary">
+                                            <i className="fa fa-lock fa-lg" aria-hidden="true"/>                                        </div>
+                                        <h3>100% Secure</h3>
+                                        <p className="text-3">Our main goal is to make our customers feel safe about their transactions. We guarantee 100% security</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </Card>
                 </div>
             </div>
         )
