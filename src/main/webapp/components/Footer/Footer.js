febc7c6381bb32a0bc71d950140a382c824f10ff
Prabin <upretiprabin7946@gmail.com>
2020-07-20 19:39:39 +0545

    Dashboard design all

--- src/main/webapp/components/Footer/Footer.js
+++ src/main/webapp/components/Footer/Footer.js
@@ -5,32 +5,74 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
 
-// intl messages
-import IntlMessages from 'Util/IntlMessages';
-
-// app config
-import AppConfig from 'Constants/AppConfig';
-
 const Footer = () => (
-   <div className="rct-footer d-flex justify-content-between align-items-center">
-      <ul className="list-inline footer-menus mb-0">
-         <li className="list-inline-item">
-            <Button component={Link} to="/app/dashboard"><IntlMessages id="sidebar.gettingStarted" /></Button>
-         </li>
-         <li className="list-inline-item">
-            <Button component={Link} to="/app/about-us"><IntlMessages id="sidebar.aboutUs" /></Button>
-         </li>
-         <li className="list-inline-item">
-            <Button component={Link} to="/app/pages/faq"><IntlMessages id="sidebar.faq(s)" /></Button>
-         </li>
-         <li className="list-inline-item">
-            <Button component={Link} to="/terms-condition"><IntlMessages id="sidebar.terms&Conditions" /></Button>
-         </li>
-         <li className="list-inline-item">
-            <Button component={Link} to="/app/pages/feedback"><IntlMessages id="sidebar.feedback" /></Button>
-         </li>
-      </ul>
-      <h5 className="mb-0">{AppConfig.copyRightText}</h5>
+   <div className="rct-footer footer d-flex justify-content-between align-items-center">
+       <div className="container">
+           <div className="row">
+               <div className="col-lg d-lg-flex align-items-center">
+                   <ul className="nav justify-content-center justify-content-lg-start text-3">
+                       <li className="nav-item"><a className="nav-link" href="#">About Us</a></li>
+                       <li className="nav-item"><a className="nav-link" href="#">Support</a></li>
+                       <li className="nav-item"><a className="nav-link" href="#">FAQ</a></li>
+                       <li className="nav-item"><a className="nav-link" href="#">How it works</a></li>
+                   </ul>
+               </div>
+               <div className="col-lg d-lg-flex justify-content-lg-end mt-3 mt-lg-0">
+                   <ul className="social-icons justify-content-center">
+                       <li className="social-icons-facebook">
+                           <a data-toggle="tooltip"
+                              href="http://www.facebook.com/hamro.remit.96"
+                              target="_blank"
+                              title="Facebook"
+                              data-original-title="Facebook">
+                               <i className="fa fa-facebook-f"/>
+                           </a>
+                       </li>
+                       <li className="social-icons-twitter">
+                           <a data-toggle="tooltip"
+                              href="http://www.twitter.com/"
+                              target="_blank"
+                              title="Twitter"
+                              data-original-title="Twitter">
+                               <i className="fa fa-twitter"/>
+                           </a>
+                       </li>
+                       <li className="social-icons-google">
+                           <a data-toggle="tooltip"
+                              href="http://www.google.com/"
+                              target="_blank" title="Google"
+                              data-original-title="Google">
+                               <i className="fa fa-google"/>
+                           </a>
+                       </li>
+                       <li className="social-icons-youtube">
+                           <a data-toggle="tooltip"
+                              href="http://www.youtube.com/"
+                              target="_blank"
+                              title="Youtube"
+                              data-original-title="Youtube">
+                               <i className="fa fa-youtube"/>
+                           </a>
+                       </li>
+                   </ul>
+               </div>
+           </div>
+           <div className="footer-copyright pt-3 pt-lg-2 mt-2">
+               <div className="row">
+                   <div className="col-lg">
+                       <p className="text-center text-lg-left mb-2 mb-lg-0">Copyright © 2020 <a href="#">Hamro Remit</a>. All
+                           Rights Reserved.</p>
+                   </div>
+                   <div className="col-lg d-lg-flex align-items-center justify-content-lg-end">
+                       <ul className="nav justify-content-center">
+                           <li className="nav-item"><a className="nav-link" href="#">Security</a></li>
+                           <li className="nav-item"><a className="nav-link" href="#">Terms</a></li>
+                           <li className="nav-item"><a className="nav-link" href="#">Privacy</a></li>
+                       </ul>
+                   </div>
+               </div>
+           </div>
+       </div>
    </div>
 );
 
--- src/main/webapp/routes/dashboard/index.js
+++ src/main/webapp/routes/dashboard/index.js
@@ -3,28 +3,18 @@
  */
 
 import React, { Component } from 'react'
-import CircularProgress from "@material-ui/core/CircularProgress/CircularProgress";
 import ErrorBoundary from "Components/ErrorBoundary/index";
 import Controller from "../../controllers/dashboardController"
 import { NotificationContainer } from "react-notifications";
 import { connect } from 'react-redux';
-// rct collapsible card
-import RctCollapsibleCard from 'Components/RctCollapsibleCard/RctCollapsibleCard';
-import IntlMessages from 'Util/IntlMessages';
-import {Card, CardTitle, FormGroup, Input} from "reactstrap";
+import {Card} from "reactstrap";
 import MatButton from '@material-ui/core/Button';
 import Select from "../../components/Select/Select"
-import {
-    VisitorAreaChartWidget,
-    SalesAreaChartWidget,
-    OrdersAreaChartWidget,
-    RecentOrdersWidget,
-    SupportRequest
-} from "Components/Widgets";
 import {
     Player, ControlBar,BigPlayButton
 } from 'video-react';
 import DownloadButton from '../../components/VideoDownloadButton/DownloadButton';
+import CircularProgress from "@material-ui/core/CircularProgress/CircularProgress";
 
 // components
 import {
@@ -70,99 +60,115 @@ class Index extends Component {
         ];
 
         return (
-            <div className="dashboard-wrapper">
-                <div className="">
-                    <SessionSlider />
+            <ErrorBoundary>
+                {loading &&
+                <div className="page-loader d-flex justify-content-center mb-30">
+                    <CircularProgress />
                 </div>
-                <div className="container send">
-                    <div className="mb-15">
-                        <h1>A better and secure way</h1>
+                }
+                {!loading &&
+                <div id={"dashboard-wrapper"} className="dashboard-wrapper">
+                    <div className="">
+                        <SessionSlider/>
                     </div>
-                    <h3>to send money from Australia to Nepal</h3>
-                    <div className="select-country mt-15 mb-15">
-                        <Select
-                            optionList={countryList}
-                            selection={"Nepal"}
-                        />
+                    <div className="container send">
+                        <div className="mb-15">
+                            <h1>A better and secure way</h1>
+                        </div>
+                        <h3>to send money from Australia to Nepal</h3>
+                        <div className="select-country mt-15 mb-15">
+                            <Select
+                                optionList={countryList}
+                                selection={"Nepal"}
+                            />
+                        </div>
+                        <h5>Today's Rate : 83.2900 NPR</h5>
+                        <MatButton className="btn btn-primary mt-10">Continue</MatButton>
                     </div>
-                    <h5>Today's Rate : 83.2900 NPR</h5>
-                    <MatButton className="btn btn-primary mt-10">Continue</MatButton>
-                </div>
-                <div className="choose mb-40">
-                    <Card body>
-                        <h2 className={"text-9 text-center mt-60"}>Why should you choose Hamro Remit?</h2>
-                        <p className="text-4 text-center mb-5">Here’s Top 4 reasons why</p>
-                        <div className="mt-30 container">
-                            <div className="row">
-                                <div className="col-sm-6 col-lg-3 mb-5 mb-lg-0">
-                                    <div className="featured-box">
-                                        <div className="featured-box-icon text-primary">
-                                            <i className="fa fa-hand-pointer-o fa-lg" aria-hidden="true"/>                                        </div>
-                                        <h3>Easy to use</h3>
-                                        <p className="text-3">We provide sophisticated UI which makes your transaction smooth and easy </p>
+                    <div className="choose mb-40">
+                        <Card body>
+                            <h2 className={"text-9 text-center mt-60"}>Why should you choose Hamro Remit?</h2>
+                            <p className="text-4 text-center mb-5">Here’s Top 4 reasons why</p>
+                            <div className="mt-30 container">
+                                <div className="row">
+                                    <div className="col-sm-6 col-lg-3 mb-5 mb-lg-0">
+                                        <div className="featured-box">
+                                            <div className="featured-box-icon text-primary">
+                                                <i className="fa fa-hand-pointer-o fa-lg" aria-hidden="true"/></div>
+                                            <h3>Easy to use</h3>
+                                            <p className="text-3">We provide sophisticated UI which makes your
+                                                transaction smooth and easy </p>
+                                        </div>
                                     </div>
-                                </div>
-                                <div className="col-sm-6 col-lg-3 mb-5 mb-lg-0">
-                                    <div className="featured-box">
-                                        <div className="featured-box-icon text-primary">
-                                            <i className="fa fa-share fa-lg" aria-hidden="true"/>                                        </div>
-                                        <h3>Faster Payments</h3>
-                                        <p className="text-3">We have developed our system to be quick. We won't keep you waiting for long</p>
+                                    <div className="col-sm-6 col-lg-3 mb-5 mb-lg-0">
+                                        <div className="featured-box">
+                                            <div className="featured-box-icon text-primary">
+                                                <i className="fa fa-share fa-lg" aria-hidden="true"/></div>
+                                            <h3>Faster Payments</h3>
+                                            <p className="text-3">We have developed our system to be quick. We won't
+                                                keep you waiting for long</p>
+                                        </div>
                                     </div>
-                                </div>
-                                <div className="col-sm-6 col-lg-3 mb-5 mb-lg-0">
-                                    <div className="featured-box">
-                                        <div className="featured-box-icon text-primary">
-                                            <i className="fa fa-dollar fa-lg" aria-hidden="true"/>                                        </div>
-                                        <h3>Lower Fees</h3>
-                                        <p className="text-3">We charge very reasonable service to our customer</p>
+                                    <div className="col-sm-6 col-lg-3 mb-5 mb-lg-0">
+                                        <div className="featured-box">
+                                            <div className="featured-box-icon text-primary">
+                                                <i className="fa fa-dollar fa-lg" aria-hidden="true"/></div>
+                                            <h3>Lower Fees</h3>
+                                            <p className="text-3">We charge very reasonable service to our customer</p>
+                                        </div>
                                     </div>
-                                </div>
-                                <div className="col-sm-6 col-lg-3 mb-5 mb-lg-0">
-                                    <div className="featured-box">
-                                        <div className="featured-box-icon text-primary">
-                                            <i className="fa fa-lock fa-lg" aria-hidden="true"/>                                        </div>
-                                        <h3>100% Secure</h3>
-                                        <p className="text-3">Our main goal is to make our customers feel safe about their transactions. We guarantee 100% security</p>
+                                    <div className="col-sm-6 col-lg-3 mb-5 mb-lg-0">
+                                        <div className="featured-box">
+                                            <div className="featured-box-icon text-primary">
+                                                <i className="fa fa-lock fa-lg" aria-hidden="true"/></div>
+                                            <h3>100% Secure</h3>
+                                            <p className="text-3">Our main goal is to make our customers feel safe about
+                                                their transactions. We guarantee 100% security</p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    </Card>
-                </div>
-                <div className="how-it-works section">
-                    <div className="container">
-                        <div className="row">
-                            <div className="video-player-wrapper col-lg-6">
+                        </Card>
+                    </div>
+                    <div className="how-it-works section">
+                        <div className="container">
+                            <div className="row">
+                                <div className="video-player-wrapper col-lg-6">
                                     <Player
                                         poster="https://reactify.theironnetwork.org/data/images/bunny-poster.jpg"
                                         src="http://media.w3.org/2010/05/bunny/movie.mp4"
                                     >
                                         <ControlBar autoHide={false}>
-                                            <DownloadButton order={7} />
-                                            <BigPlayButton position="center" />
+                                            <DownloadButton order={7}/>
+                                            <BigPlayButton position="center"/>
                                         </ControlBar>
                                     </Player>
-                            </div>
-                            <div className="col-lg-6 mt-5 mt-lg-0">
-                                <div className="ml-4">
-                                    <h2 className="text-9">How does it work?</h2>
-                                    <p className="text-4">Hamro remit is very fast, simple and safe. Here are the few steps you need to follow to make your transaction.</p>
-                                    <ul className="list-unstyled text-3 line-height-5">
-                                        <li><i className="fa fa-check mr-2"/>Create an account for free</li>
-                                        <li><i className="fa fa-check mr-2"/>Select the amount.</li>
-                                        <li><i className="fa fa-check mr-2"/>Pick your transfer method.</li>
-                                        <li><i className="fa fa-check mr-2"/>Pick existing recipient or create new one</li>
-                                        <li><i className="fa fa-check mr-2"/>Confirm and send</li>
-                                    </ul>
-                                    <a href="#" className="btn btn-outline-primary shadow-none mt-2">
-                                        Open a Free Account</a>
+                                </div>
+                                <div className="col-lg-6 mt-5 mt-lg-0">
+                                    <div className="ml-4">
+                                        <h2 className="text-9">How does it work?</h2>
+                                        <p className="text-4">Hamro remit is very fast, simple and safe. Here are the
+                                            few steps you need to follow to make your transaction.</p>
+                                        <ul className="list-unstyled text-3 line-height-5">
+                                            <li><i className="fa fa-check mr-2"/>Create an account for free</li>
+                                            <li><i className="fa fa-check mr-2"/>Select the amount.</li>
+                                            <li><i className="fa fa-check mr-2"/>Pick your transfer method.</li>
+                                            <li><i className="fa fa-check mr-2"/>Pick existing recipient or create new
+                                                one
+                                            </li>
+                                            <li><i className="fa fa-check mr-2"/>Confirm and send</li>
+                                        </ul>
+                                        <a href="#" className="btn btn-outline-primary shadow-none mt-2">
+                                            Open a Free Account</a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
+                }
+                <NotificationContainer/>
+            </ErrorBoundary>
         )
     }
 }
