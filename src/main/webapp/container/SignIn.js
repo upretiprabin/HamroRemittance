757bf1cc08d8b179bcc9f6b025daa9e7fa73fb5d
Prabin <upretiprabin7946@gmail.com>
2020-07-20 22:05:01 +0545

    Signin page new designs

--- src/main/webapp/container/SignIn.js
+++ src/main/webapp/container/SignIn.js
@@ -7,16 +7,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import { Link } from 'react-router-dom';
 import QueueAnim from 'rc-queue-anim';
-
-// components
-import {
-    SessionSlider
-} from 'Components/Widgets';
-
-// app config
 import AppConfig from 'Constants/AppConfig';
-
-// redux action
 import {
     signIn
 } from 'Actions';
@@ -55,80 +46,95 @@ class SignIn extends Component {
        const { loading } = this.props;
        return (
            <QueueAnim type="bottom" duration={2000}>
-               <div className="rct-session-wrapper">
+               <div className="app-horizontal rct-session-wrapper">
                    {loading &&
                    <LinearProgress />
                    }
-                   <AppBar position="static" className="session-header">
-                       <Toolbar>
-                           <div className="container">
-                               <div className="d-flex justify-content-between">
-                                   <div className="session-logo">
-                                       <Link to="/">
-                                           <img src={AppConfig.appLogo} alt="session-logo" className="img-fluid" width="110" height="35" />
-                                       </Link>
-                                   </div>
-                                   <div>
-                                       <a className="mr-15" onClick={() => this.onUserSignUp()}>Create New account?</a>
-                                       <Button variant="contained" className="btn-light" onClick={() => this.onUserSignUp()}>Sign Up</Button>
-                                   </div>
-                               </div>
-                           </div>
-                       </Toolbar>
-                   </AppBar>
-                   <div className="session-inner-wrapper">
-                       <div className="container">
-                           <div className="row row-eq-height">
-                               <div className="col-sm-7 col-md-7 col-lg-8">
-                                   <div className="session-body text-center">
-                                       <div className="session-head mb-30">
-                                           <h2 className="font-weight-bold">{AppConfig.brandName}</h2>
-                                       </div>
-                                       <Form>
-                                           <FormGroup className="has-wrapper">
-                                               <Input
-                                                   type="mail"
-                                                   value={email}
-                                                   name="user-mail"
-                                                   id="user-mail"
-                                                   className="has-input input-lg"
-                                                   placeholder="Enter Email Address"
-                                                   onChange={(event) => this.setState({ email: event.target.value })}
-                                               />
-                                               <span className="has-icon"><i className="ti-email"></i></span>
-                                           </FormGroup>
-                                           <FormGroup className="has-wrapper">
-                                               <Input
-                                                   value={password}
-                                                   type="Password"
-                                                   name="user-pwd"
-                                                   id="pwd"
-                                                   className="has-input input-lg"
-                                                   placeholder="Password"
-                                                   onChange={(event) => this.setState({ password: event.target.value })}
-                                               />
-                                               <span className="has-icon"><i className="ti-lock"></i></span>
-                                           </FormGroup>
-                                           <FormGroup className="mb-15">
-                                               <Button
-                                                   color="primary"
-                                                   className="btn-block text-white w-100"
-                                                   variant="contained"
-                                                   size="large"
-                                                   onClick={() => this.onUserLogin()}
-                                               >
-                                                   Sign In
-                                               </Button>
-                                           </FormGroup>
-
-                                       </Form>
-                                   </div>
-                               </div>
-                               <div className="col-sm-5 col-md-5 col-lg-4">
-                                   <SessionSlider />
-                               </div>
-                           </div>
-                       </div>
+                   <div className="container-fluid px-0 h-100">
+                        <div className="row no-gutters h-100">
+                            <div className="col-md-6">
+                                <div className="hero-wrap d-flex align-items-center h-100">
+                                    <div className="hero-mask opacity-8"/>
+                                    <div className="hero-bg hero-bg-scroll"/>
+                                    <div className="hero-content mx-auto w-100 h-100 d-flex flex-column">
+                                        <div className="row no-gutters">
+                                            <div className="col-10 col-lg-9 mx-auto">
+                                                <div className="logo mt-40 mb-5 mb-md-0">
+                                                    <a className="d-flex"
+                                                       href="/"
+                                                       title="Hamro Remit">
+                                                        <img src={AppConfig.appLogo} alt="Hamro Remit" height={50} width={100}/>
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="row no-gutters my-auto">
+                                            <div className="col-10 col-lg-9 mx-auto">
+                                                <h1 className="text-11 text-white mb-4">Welcome back!</h1>
+                                                <p className="text-4 text-white line-height-4 mb-5">We are glad to see
+                                                    you again! </p>
+                                                <p className="text-4 text-white line-height-4 mb-5">Instant money transfer from Australia to Nepal.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-md-6 d-flex align-items-center">
+                                <div className="container my-4">
+                                    <div className="row">
+                                        <div className="col-11 col-lg-9 col-xl-8 mx-auto">
+                                            <h3 className="ml-5 mb-4">Log In</h3>
+                                            <form id="loginForm" method="post">
+                                                <FormGroup className="has-wrapper">
+                                                    <Input
+                                                        type="mail"
+                                                        value={email}
+                                                        name="user-mail"
+                                                        id="user-mail"
+                                                        className="has-input input-lg"
+                                                        placeholder="Email Address"
+                                                        onChange={(event) => this.setState({ email: event.target.value })}
+                                                    />
+                                                    <span className="has-icon"><i className="ti-email"/></span>
+                                                </FormGroup>
+                                                <FormGroup className="has-wrapper">
+                                                    <Input
+                                                        value={password}
+                                                        type="Password"
+                                                        name="user-pwd"
+                                                        id="pwd"
+                                                        className="has-input input-lg"
+                                                        placeholder="Password"
+                                                        onChange={(event) => this.setState({ password: event.target.value })}
+                                                    />
+                                                    <span className="has-icon"><i className="ti-lock"/></span>
+                                                </FormGroup>
+                                                <div className="row">
+                                                    <div className="col-sm text-center mb-4">
+                                                        <a className="btn-link" href="#">Forgot Password ?</a>
+                                                    </div>
+                                                </div>
+                                                <FormGroup className="mb-15">
+                                                    <Button
+                                                        color="primary"
+                                                        className="btn-block btn-primary text-white w-100"
+                                                        variant="contained"
+                                                        size="large"
+                                                        onClick={() => this.onUserLogin()}
+                                                    >
+                                                        Log In
+                                                    </Button>
+                                                </FormGroup>
+                                            </form>
+                                            <p className="text-3 text-center text-muted">
+                                                Don't have an account?
+                                                <a className="ml-5 btn-link" onClick={() => this.onUserSignUp()}>Sign Up</a>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                    </div>
                </div>
            </QueueAnim>
