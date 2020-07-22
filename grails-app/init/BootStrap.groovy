d1197321e2fd115f05b4944510df80f9b10549e6
HarishThagunna <thagunna.harish8@gmail.com>
2020-07-22 21:46:36 +0545

    Commit #39 : Added all the TXN status values in BootStrap.groovy

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -54,6 +54,7 @@ class BootStrap {
 //        companyCharges.defaultCurrency = "NPR"
 //        companyCharges.save(flush: true, failOnError:true)
 
+        /* Transaction Status Bootstrapping*
        /* TransactionStatus transactionStatus = new TransactionStatus()
         transactionStatus.statusId = "awaitingPayments"
         transactionStatus.statusDesc = "Awaiting Payments"
@@ -63,6 +64,47 @@ class BootStrap {
         transactionStatus1.statusId = "onlineEntry"
         transactionStatus1.statusDesc = "Online Entry"
         transactionStatus1.save(flush:true, failOnError: true)*/
+
+        /* Transaction Status Bootstrapping*/
+        /*TransactionStatus transactionStatus2 = new TransactionStatus()
+        transactionStatus2.statusId = "transactionConfirmed"
+        transactionStatus2.statusDesc = "Transaction Confirmed"
+        transactionStatus2.save(flush:true, failOnError: true)
+
+        TransactionStatus transactionStatus3 = new TransactionStatus()
+        transactionStatus3.statusId = "readyToTransfer"
+        transactionStatus3.statusDesc = "Ready To Transfer"
+        transactionStatus3.save(flush:true, failOnError: true)
+
+        TransactionStatus transactionStatus4 = new TransactionStatus()
+        transactionStatus4.statusId = "checking"
+        transactionStatus4.statusDesc = "Checking"
+        transactionStatus4.save(flush:true, failOnError: true)
+
+        TransactionStatus transactionStatus5 = new TransactionStatus()
+        transactionStatus5.statusId = "pending"
+        transactionStatus5.statusDesc = "Pending"
+        transactionStatus5.save(flush:true, failOnError: true)
+
+        TransactionStatus transactionStatus6 = new TransactionStatus()
+        transactionStatus6.statusId = "declined"
+        transactionStatus6.statusDesc = "Declined"
+        transactionStatus6.save(flush:true, failOnError: true)
+
+        TransactionStatus transactionStatus7 = new TransactionStatus()
+        transactionStatus7.statusId = "refunded"
+        transactionStatus7.statusDesc = "Refunded"
+        transactionStatus7.save(flush:true, failOnError: true)
+
+        TransactionStatus transactionStatus8 = new TransactionStatus()
+        transactionStatus8.statusId = "readyForCollectionInNepal"
+        transactionStatus8.statusDesc = "Ready For Collection In Nepal"
+        transactionStatus8.save(flush:true, failOnError: true)
+
+        TransactionStatus transactionStatus9 = new TransactionStatus()
+        transactionStatus9.statusId = "paidInNepal"
+        transactionStatus9.statusDesc = "Paid In Nepal"
+        transactionStatus9.save(flush:true, failOnError: true)*/
     }
     def destroy = {
     }
