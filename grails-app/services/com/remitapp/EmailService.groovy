44e90304c6cbf372cf48829520ef087da8a7d94e
hthagunna <thagunna.harish8@gmail.com>
2020-08-17 21:11:58 +0545

    Commit #61: Dynamically append the URL in password reset link

--- grails-app/services/com/remitapp/EmailService.groovy
+++ grails-app/services/com/remitapp/EmailService.groovy
@@ -3,6 +3,7 @@ package com.remitapp
 import com.remitapp.um.User
 import grails.core.GrailsApplication
 import grails.gorm.transactions.Transactional
+import org.springframework.web.context.request.RequestContextHolder
 
 @Transactional
 class EmailService {
@@ -13,14 +14,22 @@ class EmailService {
     def emailSubject(isReset){
         def subject = "Verification Code for Hamro Remittance"
         if(isReset)
-            subject = "Password reset code for Hamro Remittance"
+            subject = "Password Reset Code for Hamro Remittance"
         return subject
     }
 
     def emailBody(isReset,token){
         def body = "Verification Code for Hamro Remittance : ${token}"
         if(isReset)
-            body = "Please click this link http://127.0.0.1:3000/reset-password?reset-code=${token}"
+        {
+            def request   = RequestContextHolder.getRequestAttributes().getRequest()
+            def protocol  = request.isSecure() ?  "https://" : "http://"
+            def domain = request?.getHeader('Host')
+            def finaUrl = protocol+domain+"/reset-password?reset-code=${token}"
+            println "finaUrl === $finaUrl"
+            body = "Please click this link ${finaUrl}"
+
+        }
         return body
     }
 
