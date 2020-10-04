4374d73ee1f6e9133e8d72036742643d5a47cda1
abisha <abisha.thapa@hotmail.com>
2020-10-04 12:59:04 +0545

    Menu Design Enhanced

--- src/main/webapp/components/HorizontalMenu/HorizontalMenu.js
+++ src/main/webapp/components/HorizontalMenu/HorizontalMenu.js
@@ -3,80 +3,44 @@
  */
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {Link} from "react-router-dom";
+import {Link, NavLink} from "react-router-dom";
 import AppConfig from 'Constants/AppConfig';
 import UserBlock from '../Sidebar/UserBlock';
 import MiniMenu from '../Sidebar/MiniMenu';
 
 class HorizontalMenu extends Component {
 
-    getHomeUrl(isDefault){
-        let homeUrl = "/home";
-        if(!isDefault && !this.isHome())
-            homeUrl = "/app/dashboard";
-        return homeUrl;
-    }
-
-    getSendUrl(isDefault,user){
-        let sendUrl = "/send";
-        if(user)
-            sendUrl = "/app/transaction";
-        return sendUrl;
-    }
-
-    getIndexPageNameAndUrl(){
-        let isHome = this.isHome();
-        let indexPageUrl = isHome?"/app/dashboard":"/home";
-        let indexPage = isHome?"DASHBOARD":"HOME";
-        return {indexPage,indexPageUrl}
-    }
-
-    isHome(){
-        return location.pathname.includes("home");
-    }
-
-    isDashboard(){
-        return location.pathname.includes("dashboard");
-    }
-
-    isDescriptionPage(){
-        return location.pathname.includes("about-us") || location.pathname.includes("how-it-works");
-    }
-
     render() {
         const {user,isDefault,history} = this.props;
-        let homeUrl = this.getHomeUrl(isDefault);
-        let sendUrl = this.getSendUrl(isDefault,user);
-        let {indexPage,indexPageUrl} = this.getIndexPageNameAndUrl();
+
         return (
             <div className="horizontal-menu">
                 <div className="container">
                     <div className = "rct-header d-flex">
                         <div className="site-logo mr-15">
-                            <a href={homeUrl} className="logo-normal">
+                            <a href={"/home"} className="logo-normal">
                                 <img src={AppConfig.appLogo} className="" alt="site-logo" width="100" height="55" />
                             </a>
                         </div>
                         <MiniMenu isDefault={isDefault} user={user} propClassName="hideHamBtn"/>
                         <div className={"mt-15 menus"}>
                             <ul className="list-inline footer-menus mb-0">
-                                {!this.isHome() && !isDefault &&
-                                    <li className="list-inline-item">
-                                        <Link to={"/app/dashboard"} ><span className={"header-menu"}>DASHBOARD</span></Link>
-                                    </li>
-                                }
                                 <li className="list-inline-item">
-                                    <Link to={sendUrl}><span className={"header-menu"}>SEND</span></Link>
+                                    <NavLink to={{pathname: "/home"}} activeClassName="selected" exact className={"header-menu"}>HOME</NavLink>
                                 </li>
                                 <li className="list-inline-item">
-                                    <Link to={"/how-it-works"} ><span className={"header-menu"}>HOW IT WORKS</span></Link>
+                                    <NavLink to={{pathname: "/how-it-works"}} activeClassName="selected" exact className={"header-menu"}>HOW IT WORKS</NavLink>
                                 </li>
                                 <li className="list-inline-item">
-                                    <Link to={"/about-us"}><span className={"header-menu"}>ABOUT US</span></Link>
+                                    {!user && <NavLink to={{pathname:"/send"}} activeClassName="selected" exact className={"header-menu"}>SEND</NavLink>}
+                                    {user && <NavLink to={{pathname:"/app/transaction"}} activeClassName="selected" exact className={"header-menu"}>SEND</NavLink>}
+                                </li>
+                                <li className="list-inline-item">
+                                    <NavLink to={{pathname:"/about-us"}} activeClassName="selected" exact className={"header-menu"}>ABOUT US</NavLink>
                                 </li>
                                 {!user &&
                                     <li className="list-inline-item">
-                                        <Link to={"/signin"}><span className={"header-menu"}>LOG IN</span></Link>
+                                        <NavLink to={{pathname:"/signin"}} activeClassName="selected" exact className={"header-menu"}>LOG IN</NavLink>
                                     </li>
                                 }
                                 {!user &&
@@ -84,14 +48,9 @@ class HorizontalMenu extends Component {
                                         <Link to={"/signup"} className={"btn btn-primary header-menu-btn"}><span className={"header-menu"}>SIGN UP</span></Link>
                                     </li>
                                 }
-                                {user && this.isDescriptionPage() && !this.isDashboard() &&
+                                {user &&
                                     <li className="list-inline-item">
-                                        <Link to={"/app/dashboard"}><span className={"header-menu"}>DASHBOARD</span></Link>
-                                    </li>
-                                }
-                                {user && !isDefault &&
-                                    <li className="list-inline-item sign-up">
-                                        <Link to={indexPageUrl} className={"btn btn-primary header-menu-btn"}><span className={"header-menu"}>{indexPage}</span></Link>
+                                        <NavLink to={{pathname:"/app/dashboard"}} activeClassName="selected" exact className={"header-menu"}>DASHBOARD</NavLink>
                                     </li>
                                 }
                                 {user &&
--- src/main/webapp/components/Sidebar/MiniMenu.js
+++ src/main/webapp/components/Sidebar/MiniMenu.js
@@ -1,78 +1,63 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {Link} from "react-router-dom";
-
-import { Dropdown, DropdownToggle, DropdownMenu } from 'reactstrap';
-import CustomIcon from '../../components/CustomIcon/index';
-import classNames from 'classnames';
+import {NavLink} from "react-router-dom";
+import {IconButton, Divider, Drawer, ListItem, ListItemText} from '@material-ui/core';
 
 class MiniMenu extends Component {
 
     state = {
-        userDropdownMenu: false
+        top: false
     };
 
-    toggleUserDropdownMenu() {
-        this.setState({ userDropdownMenu: !this.state.userDropdownMenu });
-    }
-
-    getSendUrl(isDefault,user){
-        let sendUrl = "/send";
-        if(user)
-            sendUrl = "/app/transaction";
-        return sendUrl;
-    }
-
-    pageActive(){
-        const _path = location.pathname;
-        let menuActive = 0;
-        if(_path.includes("send") || _path.includes("transaction")) menuActive = 1;
-        if(_path.includes("how-it-works")) menuActive = 2;
-        if(_path.includes("about-us")) menuActive = 3;
-        if(_path.includes("dashboard")) menuActive = 4;
-        if(_path.includes("home")) menuActive = 5;
-        return menuActive;
-    }
+    toggleDrawer = (open) => () => {
+        this.setState({
+            top: open
+        });
+    };
 
     render() {
         const {isDefault, user, propClassName} = this.props;
-        let sendUrl = this.getSendUrl(isDefault,user);
-        let pageNo = this.pageActive();
 
         return (
-            <Dropdown
-            isOpen={this.state.userDropdownMenu}
-            toggle={() => this.toggleUserDropdownMenu()}
-            className="list-inline-item d-flex"
-            >
-                <DropdownToggle
-                tag="div"
-                className={classNames("d-flex align-items-center",propClassName)}
-                >
-                    <i className="fa fa-bars fa-2x" aria-hidden="true"></i>
-                </DropdownToggle>
-                <DropdownMenu className="header-nav">
-                    <ul className="list-unstyled mb-0">
-                        <li className={"p-1 border-bottom rounded-top"}>
-                            <Link to={sendUrl} className={classNames("dropdown-item",pageNo==1?"active":"")}><span className={"header-menu"}>SEND</span></Link>
-                        </li>
-                        <li className="p-1 border-top">
-                            <Link to={"/how-it-works"} className={classNames("dropdown-item",pageNo==2?"active":"")}><span className={"header-menu"}>HOW IT WORKS</span></Link>
-                        </li>
-                        <li className="p-1 border-top">
-                            <Link to={"/about-us"} className={classNames("dropdown-item",pageNo==3?"active":"")}><span className={"header-menu"}>ABOUT US</span></Link>
-                        </li>
+           <div>
+                <IconButton color="primary" aria-label="Menu Toggle" className={propClassName} onClick={this.toggleDrawer(true)}>
+                    <i className="fa fa-bars" aria-hidden="true"></i>
+                </IconButton>
+                <Drawer anchor="top" open={this.state.top} onClose={this.toggleDrawer(false)}>
+                  <div tabIndex={0} role="button"
+                     onClick={this.toggleDrawer(false)}
+                     onKeyDown={this.toggleDrawer(false)}>
+                        <ListItem button component={NavLink} to={{pathname: "/home"}} activeClassName="selected" exact>
+                          <ListItemText primary="HOME" />
+                        </ListItem>
+                        <Divider />
+                        <ListItem button component={NavLink} to={{pathname: "/how-it-works"}} activeClassName="Mui-selected" exact>
+                          <ListItemText primary="HOW IT WORKS" />
+                        </ListItem>
+                        <Divider />
                         {user &&
-                            <li className="p-1 border-top">
-                                <Link to={"/app/dashboard"} className={classNames("dropdown-item",pageNo==4?"active":"")}><span className={"header-menu"}>DASHBOARD</span></Link>
-                            </li>
+                            <ListItem button component={NavLink} to={{pathname: "/app/transaction"}} activeClassName="Mui-selected" exact>
+                              <ListItemText primary="SEND" />
+                            </ListItem>
                         }
-                        <li className="p-1 border-top">
-                            <Link to={"/home"} className={classNames("dropdown-item",pageNo==5?"active":"")}><span className={"header-menu"}>HOME</span></Link>
-                        </li>
-                    </ul>
-                </DropdownMenu>
-            </Dropdown>
+                        {!user &&
+                            <ListItem button component={NavLink} to={{pathname: "/send"}} activeClassName="Mui-selected" exact>
+                              <ListItemText primary="SEND" />
+                            </ListItem>
+                        }
+                        <Divider />
+                        <ListItem button component={NavLink} to={{pathname: "/about-us"}} activeClassName="Mui-selected" exact>
+                          <ListItemText primary="ABOUT US" />
+                        </ListItem>
+                        {user && <Divider />}
+                        {user &&
+                            <ListItem button component={NavLink} to={{pathname: "/app/dashboard"}} activeClassName="Mui-selected" exact>
+                              <ListItemText primary="DASHBOARD" />
+                            </ListItem>
+                        }
+                  </div>
+                </Drawer>
+            </div>
         );
     }
 }
